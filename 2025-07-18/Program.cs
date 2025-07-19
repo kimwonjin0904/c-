aebd5bdad45b13e0f65ab191240ecaad7b1eb1cf
LG <LG@DESKTOP-R7QVG5C>
2025-07-19 14:24:36 +0900

    초기 커밋.

---
+++ 2025-07-18/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;   //system만 Console.WriteLine()으로 사용 가능
+
+
+namespace _2025_07_18
+   
+{
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+
+            object a = "Hello, World!";
+            object b = 1234;
+            object c = 'a';
+
+            //WriteLine(a+b);
+            
+            WriteLine(a + "\n" + b + "\n" + c);
+
+        }
+    }
+}
