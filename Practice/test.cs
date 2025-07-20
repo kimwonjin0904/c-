c6c4f292d9cc746637233a3b451725fb48f5e0ae
원진 김 <kwj00904@naver.com>
2025-07-21 00:16:54 +0900

    수정

--- Practice/test.cs
+++ Practice/test.cs
@@ -5,18 +5,12 @@ public class 자리표시자
     public static void Main()
     {
 
-        int a = 10;
-        float b = 12.3f;
-        Console.WriteLine($"정수: {a}, 실수: {b}");
+        const int a = 10;   
+        const int b = 20;
 
-        string s = a.ToString(); // 문자열 "10"
-        int x = int.Parse("123"); // 숫자 123
-        
-        Console.WriteLine(s);
-        Console.WriteLine(x);
+        Console.WriteLine(a);
 
 
 
-
-    }
+    } 
 }
