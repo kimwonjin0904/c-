b9cd7ccecbed55b0e5eeaf6254dc6799722e67c4
원진 김 <kwj00904@naver.com>
2025-07-21 16:02:23 +0900

    수정

---
+++ 2025-07-18/상수_const_80/상수.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class 상수_const_80
+{
+    public static void Main()
+    {
+
+        const int a = 10;
+        
+
+        Console.WriteLine(a);
+        Console.WriteLine($"a값:{a}");
+
+
+
+    }
+}
--- 2025-07-18/상수_const_80/상수2.cs
+++ 2025-07-18/상수_const_80/상수2.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 
-public class 자리표시자
+public class 상수2
 {
     public static void Main()
     {
 
         int MAX_INT = 123;
         Console.WriteLine(MAX_INT);  //123
-        
+
         int a = 3;
         a = 4;
         Console.WriteLine(a);   //4  const가 아니고 int 이기 떄문에 가능
 
-       // const int b = 5;
-       // b = 6;
-       //이렇게 할시 const는 값을 지정할 수 없음
-       //오류생김
+        // const int b = 5;
+        // b = 6;
+        //이렇게 할시 const는 값을 지정할 수 없음
+        //오류생김
 
 
 
-    } 
+    }
 }
