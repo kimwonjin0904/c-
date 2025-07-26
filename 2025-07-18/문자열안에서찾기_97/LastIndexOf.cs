126fb9030938c81419b62f44a0d9768e2568c0d2
원진 김 <kwj00904@naver.com>
2025-07-26 23:41:44 +0900

    수정

--- 2025-07-18/문자열안에서찾기_97/LastIndexOf.cs
+++ 2025-07-18/문자열안에서찾기_97/LastIndexOf.cs
@@ -5,9 +5,12 @@ public class LastIndexOf
 
     public static void Main()
     {
-       
 
 
+        //LastIndexOf 문자열
+        string name = "김원진";
+        int a = name.LastIndexOf("김");
+        Console.WriteLine(a);// 0
 
 
     }
--- Practice/test.cs
+++ Practice/test.cs
@@ -5,12 +5,17 @@ public class 자리표시자
 
     public static void Main()
     {
-        //LastIndexOf 문자열
-        string name = "김원진";
-        int a = name.LastIndexOf("김");
-        Console.WriteLine(a);//
+        decimal d = 12.34m;
+        Console.WriteLine("{0}",d);
+
+        string age = "26";
+        int change = int.Parse(age); // 문자열을 int로 변환
+        Console.WriteLine(age);//
 
         
+
+
+
     }
 
 }
