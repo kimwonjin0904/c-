e8fa74f450045cf7506cc9c94380fc1c7ffd26b8
원진 김 <kwj00904@naver.com>
2025-07-29 23:49:57 +0900

    수정

---
+++ 2025-07-18/기타/정수변환.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class 정수변환
+{
+    public static void Main()
+    {
+        // 정수 형식 변환하는 세 가지 방법
+
+        // 1. Convert.ToInt32()
+        string age = "26";
+        int changeAge = Convert.ToInt32(age);
+        Console.WriteLine(changeAge);
+        Console.WriteLine($"{age} - {changeAge.GetType()}");
+        Console.WriteLine("GetType(): {0}", changeAge.GetType());
+
+        // 2. int.Parse()
+        int age2 = int.Parse(age);
+        Console.WriteLine($"{age2} - {age.GetType()}");
+
+        // 3. int.TryParse()
+        int age3;
+        bool isParsed = int.TryParse(age, out age3);
+        Console.WriteLine(isParsed ? $"성공: {age3}" : "실패");
+    }
+}
