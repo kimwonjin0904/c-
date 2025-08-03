07e70c0d07cf2589ea0af3d104d40d2c088acafc
원진 김 <kwj00904@naver.com>
2025-08-03 23:15:09 +0900

    수정

--- 2025-07-18/C#_교과서/PadLeft.cs
+++ 2025-07-18/C#_교과서/PadLeft.cs
@@ -6,7 +6,6 @@ public class PadLeft
     {
         //PadLeft();
         string s = "안녕";
-
         Console.Write($"{s.PadLeft(3, '0')}"); // "안녕"을 길이 3으로 맞추고 왼쪽에 '0'을 채움 
                                                 //padLight이면 안녕000
                                                 //''까먹지 말고 쓰기
