d4025e9b9535942ebe90c669cf468664767ef15e
원진 김 <kwj00904@naver.com>
2025-08-03 13:06:35 +0900

    수정

--- Practice/test.cs
+++ Practice/test.cs
@@ -4,11 +4,9 @@ public class 자리표시자
 {
     public static void Main()
     {
-        //PadLeft();
-        string s = "안녕";
-        
-        Console.Write($"{s.PadLeft(3,'0')}"); // "안녕"을 길이 3으로 맞추고 왼쪽에 '0'을 채움
-
+        var s = Console.ReadLine();
+        var c = Convert.ToChar(Console.Read());
+        Console.WriteLine($"s:{s},{s.GetType()} -- c:{c},{c.GetType()}");
     } 
     
 }
