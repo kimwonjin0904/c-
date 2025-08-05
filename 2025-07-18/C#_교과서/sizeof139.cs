84d340f8fd642505b48088172260bab4337888bb
원진 김 <kwj00904@naver.com>
2025-08-05 23:56:27 +0900

    수정

---
+++ 2025-07-18/C#_교과서/sizeof139.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_07_18.C__교과서
+{
+    internal class sizeof139
+    {
+    }
+}
--- Practice/test.cs
+++ Practice/test.cs
@@ -5,30 +5,9 @@ public class 자리표시자
 {
     public static void Main()
     {
-        //not연산자
-        // (1 == 1) -> true
-        // (1 != 1) -> false
-        //!(1 == 1) -> false
-        //!(1 != 1) -> true
-
-        var i = 3;
-        var j = 5;
-        var r = false;
-
-        r = (i == 3) && (j != 3);// true && true -> true  ,i가 3이고 j가 3이 아니므로 true
-        WriteLine(r);
-
-        r = (i != 3) && (j != 3);// false && true -> false ,i가 3이므로 false
-        WriteLine(r);
-
-        r = (i < 5);   // true , i가 5보다 작으므로 true
-        WriteLine(!r); // i가 5보다 작으므로 true, !r는 false
-
-        Console.WriteLine(false && true);// false, 둘 중 하나라도 false이면 false
-        Console.WriteLine((1 == 1) || (1 != 1)); // true, 둘 중 하나라도 true이면 true
-        Console.WriteLine(!(1 == 1));// false, 1이 1과 같으므로 true, !true는 false
-
-
+        //sizeof 연산자
+        //Console.WriteLine("sizeof(int) = {0}", sizeof(int));//출력값:4
+       
     }
 
 }
