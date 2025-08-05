6357de78ebd6134f01d73d717d94969743ac9d93
원진 김 <kwj00904@naver.com>
2025-08-05 20:05:43 +0900

    수정

---
+++ 2025-07-18/C#_교과서/관계형연산자.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+public class 관계형연산자
+{
+    public static void Main()
+    {
+        int x = 3;
+        int y = 5;
+        Console.WriteLine(x == y);//false
+
+        Console.WriteLine(x != y);//true !는 아닐떄
+
+        Console.WriteLine(x > y);//false x가 y보다 클때 출력값:false
+
+        Console.WriteLine(x <= y);//true x가 y보다 작거나 같을때 출력값:true
+        
+
+
+    }
+
+}
--- Practice/test.cs
+++ Practice/test.cs
@@ -5,9 +5,22 @@ public class 자리표시자
 {
     public static void Main()
     {
+<<<<<<< HEAD
         int num = 10;
         ++num;
         Console.WriteLine(num);
+=======
+        int x = 3;
+        int y = 5;
+        Console.WriteLine(x==y);//false
+
+        Console.WriteLine(x != y);//true !는 아닐떄
+
+        Console.WriteLine(x>y);//false x가 y보다 클때 출력값:false
+
+        Console.WriteLine(x<=y);//true x가 y보다 작거나 같을때 출력값:true
+
+>>>>>>> 수정
 
         //
 
