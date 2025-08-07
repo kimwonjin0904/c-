2a2979afd61b2da1d0d5e591f9a93fe06a04f9c8
원진 김 <kwj00904@gmail.com>
2025-08-07 13:44:06 +0900

    수정

---
+++ 2025-07-18/C#_교과서/while문_181/1부터100까지합.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_07_18.C__교과서.while문_181
+{
+    internal class _1부터100까지합
+    {
+    }
+}
--- Practice/test.cs
+++ Practice/test.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using static System.Console;
 
 public class 가로구구단
 {
     public static void Main()
     {
-        int count = 0;
-        while(count < 3)
+        const int N = 100;
+              int i = 1;
+              int sum = 0;
+        while (i <= N)
         {
-            Console.WriteLine("안녕3번출력");
-            count++;
+            sum = sum + i; // i를 sum에 더함 (1+0=1)..
+           // Console.WriteLine($"1~100까지합: {sum}");
+            i++;
         }
+        Console.WriteLine($"1~100까지합: {sum}");
     }
 }
 
