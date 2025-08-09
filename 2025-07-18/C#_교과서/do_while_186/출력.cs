419d26448da63e55fd5c1e9a020b9fd7d4274130
원진 김 <kwj00904@gmail.com>
2025-08-09 15:16:12 +0900

    메세지내용

---
+++ 2025-07-18/C#_교과서/do_while_186/출력.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_07_18.C__교과서.do_while_186
+{
+    internal class 출력
+    {
+    }
+}
--- Practice/test.cs
+++ Practice/test.cs
@@ -6,16 +6,15 @@ public class 가로구구단
 {
     public static void Main()
     {
-        const int N = 100;
-              int i = 1;
-              int sum = 0;
-        while (i <= N)
+        int count = 0;   //초기식
+        do
         {
-            sum = sum + i; // i를 sum에 더함 (1+0=1)..
-           // Console.WriteLine($"1~100까지합: {sum}");
-            i++;
+            Console.WriteLine("안녕하세요"); //실행문
+            count++; //증감식
         }
-        Console.WriteLine($"1~100까지합: {sum}");
+
+        while (count < 3);
     }
 }
+
 
