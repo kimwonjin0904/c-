b3427c0df50fceedd9e3295fb7d47b557078d5e7
원진 김 <kwj00904@gmail.com>
2025-08-14 16:42:26 +0900

    tw

---
+++ 2025-07-18/메서드_227/절댓값.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Channels;
+using static System.Console;
+
+public class 절댓값
+{
+    static int Abs(int num)
+    {
+        return (num < 0) ? -num : num;
+    }
+
+    static void Main()
+    {
+        int num = -21;
+        int abs = Abs(num); // Abs 함수 호출
+        Console.WriteLine($"{num}의 절대값:{abs}");
+    }
+}
+
+
+
+
+
+
+
