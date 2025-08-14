55551a5ea7764a60c68a7a5fb0adf4a3bdbe3f7c
원진 김 <kwj00904@gmail.com>
2025-08-14 13:51:09 +0900

    메세지내용

--- Practice/test.cs
+++ Practice/test.cs
@@ -8,10 +8,19 @@ public class 반환값
         return "반환값"; //return 키워드로 단순 문자열 반환
     }
 
-    static void Main()
+    static string Asp()
     {
         string returnValue = GetString(); // 함수 호출
         Console.WriteLine(returnValue); //반환값을 returnValue 변수에 저장하고 출력
+        return returnValue; // 반환값을 다시 반환
+    }
+
+    static void Main()
+    {
+        string reuslt = Asp();
+       // string result = "";
+      //  result = Asp();
+        Console.WriteLine(reuslt);
     }
 }
 
