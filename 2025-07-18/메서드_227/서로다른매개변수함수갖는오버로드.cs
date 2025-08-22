279e49931eb37c4f8ca905ecf1f20f60e5b96ede
원진 김 <kwj00904@gmail.com>
2025-08-22 23:46:01 +0900

    메세지내용

--- 2025-07-18/메서드_227/서로다른매개변수함수갖는오버로드.cs
+++ 2025-07-18/메서드_227/서로다른매개변수함수갖는오버로드.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.ExceptionServices;
+using static System.Console;
 
-namespace _2025_07_18.메서드_227
+public class 서로다른매개변수함수갖는오버로드
 {
-    internal class 서로다른매개변수함수갖는오버로드
+    static void Multi()
     {
+        Console.WriteLine("안녕");
+    }
+
+    static void Multi(string message)
+    {
+        Console.WriteLine(message);
+    }
+    static void Multi(string message, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main()
+        {
+            Multi(); // 매개변수 없는 함수 호출
+            Multi("2번쨰"); // 문자열 매개변수 함수 호출
+            Multi("3번쨰", 3); // 문자열과 정수 매개변수 함수 호출
+        }
     }
 }
+
