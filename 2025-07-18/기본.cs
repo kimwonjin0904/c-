c30ea652c23cd7371676b4024ad015f53caa8547
원진 김 <kwj00904@gmail.com>
2025-08-23 22:33:54 +0900

    uu

--- 2025-07-18/기본.cs
+++ 2025-07-18/기본.cs
@@ -8,13 +8,7 @@ public class 화살표함수
 {
     static void Main()
     {
-        HI();
-        Plus(3,5);
+
+
     }
-    
-        static void HI() => Console.WriteLine("hi");
-        static void Plus(int a, int b) => Console.WriteLine(a + b);
-
-
-    
 }
---
+++ 2025-07-18/메서드_227/로컬함수.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.ExceptionServices;
+using static System.Console;
+
+public class 로컬함수
+{
+    static void Main()
+    {
+        void Display(string text)   //Display 메서드 Main 메서드 안에 Display라는 새로운 메서드를 선언했습니다.
+                                    //이 메서드는 text라는 문자열을 입력받아 어떤 행동(Console.WriteLine)을 수행
+        {
+            Console.WriteLine(text);
+        }
+
+        Display("안녕하세요");
+    }
+}
