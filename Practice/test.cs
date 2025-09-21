f3b7fa1994affe6f0568869ba2098736b2e76a41
원진 김 <kwj00904@gmail.com>
2025-09-21 23:52:03 +0900

    ss

--- Practice/test.cs
+++ Practice/test.cs
@@ -1,24 +1,14 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Numerics;
-using System.Runtime.ExceptionServices;
-using System.Runtime.InteropServices;
-using static System.Console;
 
-
-//열거형
-enum Status : byte { Inactive =0,
-                      Active = 1,
-                      Pending = 2}
-
-public class  EnumIndexChange
+class Arrow
 {
+    static void Log(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+
     static void Main()
     {
-        Status curreuntStaus = Status.Active;  //열거형 변수 선언 currentStatus에 1 할당
-        WriteLine($"현재상태: {curreuntStaus} {(int)curreuntStaus}");//출력: 현재상태: Active 1
-        WriteLine((int)curreuntStaus);//출력: 1
+        Log("gd");
     }
 }
